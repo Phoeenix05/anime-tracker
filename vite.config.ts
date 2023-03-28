1d9ffef3af5fc5bdebbb68904866c5637cbe4974
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-03-28 11:11:15 +0300

    added eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import devtools from 'solid-devtools/vite';
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import devtools from 'solid-devtools/vite'
+import { defineConfig } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
 
 export default defineConfig({
-  plugins: [solidPlugin(), devtools({ autoname: true })],
-  server: {
-    port: 8000,
-  },
-  build: {
-    target: 'esnext',
-  },
-});
+	plugins: [solidPlugin(), devtools({ autoname: true })],
+	server: {
+		port: 8000,
+	},
+	build: {
+		target: 'esnext',
+	},
+})

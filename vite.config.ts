9e6ce391f8b7f81c35073b393c0e62f11f2ff4b3
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2023-03-28 10:33:23 +0300

    added solidjs devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import devtools from 'solid-devtools/vite';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [solidPlugin(), devtools({ autoname: true })],
   server: {
-    port: 3000,
+    port: 8000,
   },
   build: {
     target: 'esnext',
